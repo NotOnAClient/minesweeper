a0e13dcfdb2c20c54e0444ee07b155a79d65ff76
NotOnAClient <50760574+NotOnAClient@users.noreply.github.com>
2021-11-25 15:59:44 +0800

    Highlighting half-working

--- js/game.js
+++ js/game.js
@@ -4,72 +4,103 @@ var title = document.getElementById("title");
 
 canvas.style.backgroundColor = 'lightgreen';
 
-var mouseX = 0;
-var mouseY = 0;
-canvas.onmousemove = function (e) {
-    // important: correct mouse position:
-    var rect = this.getBoundingClientRect()
-    mouseX = e.clientX - rect.left;
-    mouseY = e.clientY - rect.top;
-    //console.log(mouseX, mouseY);
-};
-
-
-var tileSize = 50;
-var bombCount = 10;
-var tileCountRow = (canvas.width - 4) / tileSize;
-var tileCountColumn = (canvas.height - 4) / tileSize;
-
-var tileX = 0;
-var tileY = 0;
+function getMousePos(canvas, evt) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+        x: evt.clientX - rect.left,
+        y: evt.clientY - rect.top
+    };
+}
 
 var tileList = [];
-function drawRow() {
-    let rowList = [];
-    for (let i = 0; i < tileCountRow; i++) {
-        rowList[i] = new tileClass(ctx, tileX, tileY, tileSize, false);
-        //rowList[i].draw();
-        tileX += tileSize;
+var mouseX;
+var mouseY;
+canvas.addEventListener('mousemove', (e) => {
+    var mousePos = getMousePos(canvas, e);
+    mouseX = mousePos.x;
+    mouseY = mousePos.y;
+    //console.log(mouseX, mouseY);
+})
+
+class Tile {
+    constructor(x, y, size) {
+        this.count;
+        this.x = x;
+        this.y = y;
+        this.size = size;
+        ctx.strokeStyle = 'black';
+        ctx.fillStyle = 'blue';
+        ctx.lineWidth = 2;
     }
-    return rowList;
-}
 
-function generateLevel() {
-    for (let i = 0; i < tileCountColumn; i++) {
-        tileList[i] = drawRow();
-        tileX = 0;
-        tileY += tileSize;
+
+    draw() {
+        ctx.rect(this.x, this.y, this.size, this.size);
+        ctx.fill();
+        ctx.stroke();
+    }
+
+    update() {
+        let hover = mouseX >= this.x && mouseX <= this.x + this.size && mouseY >= this.y && mouseY <= this.y + this.size;
+        if (hover) {
+            ctx.fillStyle = 'yellow';
+            this.draw();
+        }
+
     }
 }
 
-generateLevel();
-console.log(tileList);
-
-var mouseDown = false;
-canvas.onmousedown = function (e) {
-    mouseDown = true;
-}
-canvas.onmouseup = function (e) {
-    mouseDown = false;
-}
-
-var i = 0;
-window.requestAnimationFrame(loop);
-function loop(timeStamp) {
-    for (let i = 0; i < tileList.length; i++) {
-        tileList[i][i].mouseX = mouseX;
-        tileList[i][i].mouseY = mouseY;
+class Level {
+    constructor(tileSize) {
+        this.tileCountWidth = canvas.width / tileSize;
+        this.tileCountHeight = canvas.height / tileSize;
     }
-    //console.log(tileList);
-    if (i >= tileList.length) {
-        i = 0;
-    }
-    tileList[0][i].draw();
-    tileList[0][i].update();
-    tileList[0][i].clicked();
-    //console.log(tileList[i]);
 
-    i++;
-    window.requestAnimationFrame(loop);
+    createLevel() {
+        let x = 0;
+        let y = 0;
+        for (let i = 0; i < this.tileCountWidth * this.tileCountHeight; i++) {
+            let tile = new Tile(x, y, tileSize)
+            tile.count = i + 1;
+            tileList.push(tile);
+            x += tileSize;
+            if (x >= this.tileCountWidth * tileSize) {
+                x = 0;
+                y += tileSize;
+            }
+        }
+        console.log(tileList);
+    };
+
+    drawLevel() {
+        for (let i = 0; i <= tileList.length - 1; i++) {
+            //console.log(tileList[i]);
+            ctx.beginPath();
+            tileList[i].draw();
+            //ctx.fillStyle = ctx.isPointInPath(this.x, this.y) ? 'yellow' : 'blue';
+            //ctx.fill();
+        }
+        ctx.closePath();
+    };
+
+    update() {
+        for (let i = 0; i <= tileList.length - 1; i++) {
+            tileList[i].update();
+        }
+    }
 }
+
+const tileSize = 20;
+var level = new Level(tileSize);
+level.createLevel();
+level.drawLevel();
+window.requestAnimationFrame(gameLoop);
+function gameLoop(timeStamp) {
+    //console.log(timeStamp);
+    level.update();
+    //ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    window.requestAnimationFrame(gameLoop);
+}
+
 
